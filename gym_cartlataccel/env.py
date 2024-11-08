8e92a4321014667e1ce923476fd4b00e571f6096
Ellen Xu <ellen2imagine@gmail.com>
2024-11-08 15:58:42 -0800

    change environment dynamics

--- gym_cartlataccel/env.py
+++ gym_cartlataccel/env.py
@@ -90,13 +90,33 @@ class BatchedCartLatAccelEnv(gym.Env):
     action = action.squeeze()
     noisy_action = self.noise_model.add_lat_noise(self.curr_step, action)
 
+    # CHANGE HERE
     new_a = noisy_action * self.force_mag # steer * force
-    new_x = 0.5 * new_a * self.tau**2 + v * self.tau + x
+    friction_term = 0.1 * np.sign(v) * v**2  # Quadratic drag
+    nonlinear_coupling = 0.05 * np.sin(2 * x) * v  # Position-velocity coupling
+    stiffness_term = 0.15 * np.sin(3 * x)  # Nonlinear position dependent force
+    
+    # Updated dynamics with nonlinear terms
+    new_x = (0.5 * new_a * self.tau**2 + 
+             v * self.tau + 
+             x - 
+             friction_term * self.tau - 
+             nonlinear_coupling * self.tau -
+             stiffness_term * self.tau)
+    
     new_x = np.clip(new_x, -self.max_x, self.max_x)
-    new_v = new_a * self.tau + v
+    new_v = ((new_a - friction_term - nonlinear_coupling - stiffness_term) * 
+             self.tau + v)
     new_x_target = self.x_targets[:, self.curr_step]
 
     self.state = np.stack([new_x, new_v, new_x_target], axis=1)
+
+    # new_x = 0.5 * new_a * self.tau**2 + v * self.tau + x
+    # new_x = np.clip(new_x, -self.max_x, self.max_x)
+    # new_v = new_a * self.tau + v
+    # new_x_target = self.x_targets[:, self.curr_step]
+
+    # self.state = np.stack([new_x, new_v, new_x_target], axis=1)
 
     error = abs(new_x - new_x_target)
     reward = -error/self.max_episode_steps # scale reward
